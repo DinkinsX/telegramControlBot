4bb4a25b47315657654d0a2a745a36f29518d1cd
Daniel <dan767@yandex.ru>
2021-07-23 23:54:02 +0700

    fewVoiceCommand

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const telegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
-const APIs = require('./API.js');
-const telegramAPI = APIs.telegramAPI;
-const yandexAPI = APIs.yandexAPI;
+const API = require('./API.js');
+const fs = require('fs');
+const telegramAPI = API.telegramAPI;
+const yandexAPI = API.yandexAPI;
 
 const bot = new telegramBot(telegramAPI, {polling: true});
 
 bot.on('voice', (msg) => {
-    //преобразовыввает файл в поток
+
     const chatId = msg.chat.id;
+    //преобразовыввает файл в поток
     const stream = bot.getFileStream(msg.voice.file_id);
     let chunks = [];
     stream.on('data', chunk => chunks.push(chunk));
     stream.on('end', () => {
+
         const axiosConfig = {
             method: 'POST',
             url: 'https://stt.api.cloud.yandex.net/speech/v1/stt:recognize',
@@ -23,14 +26,40 @@ bot.on('voice', (msg) => {
         };
 
         axios(axiosConfig).then(response => {
-            console.log(response.data);
-            bot.sendMessage(chatId, `Вы сказали: ${response.data.result}`);
+
+            const command = response.data.result;
+            bot.sendMessage(chatId, `Вы сказали: ${command}`);
+            //Команды для бота
+            if (command === 'Время') {
+
+                let timeNow = writeTime();
+                bot.sendMessage(chatId, `Время: ${timeNow}`);
+
+            }
+            
+            if (command === 'Привет') {
+                bot.sendMessage(chatId, `Здравствуй, ${msg.chat.username}`);
+            }
+
         }).catch(err => {
             console.log('Ошибка распознавания: ', err);
         });
+
     });
     //  // ссылка на файл
     //   bot.getFileLink(msg.voice.file_id).then((url) => {
     //       console.log(url);
     //   });
+
 });
+
+function writeTime(){
+
+    const timeNow = new Date();
+    fs.appendFile('timeWrite.txt', `\n${timeNow}`, (err) => {
+        if(err) throw err;
+        console.log('Дата была добавлена');
+    });
+    return timeNow;
+
+}
